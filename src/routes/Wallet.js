ac2b72bd20ccabbadc36b60d78934c00ae4d7462
root <root@localhost.localdomain>
2020-02-10 17:00:26 +0100

    removed exchange value bug

--- src/routes/Wallet.js
+++ src/routes/Wallet.js
@@ -4,14 +4,14 @@ import { DataContext } from "../context/DataContext";
 import TradeButtons from "../components/TradeButtons"
 const Wallet = () => {
     const dataCon = useContext(DataContext);
-    
+    const exchangeValue = dataCon.ballance*dataCon.exchangeValue.toFixed(2)
     return (
         <div className="wallet">
             
 
             <div className="ammount">
         <div id="eos"><h1>{dataCon.ballance}</h1><img alt="EOS-ICON" src="https://res.cloudinary.com/munnotubbel/image/upload/v1581182752/wombat/5a52232c2f93c7a8d5137fdd_izhtxc.png"/></div>
-    <div id="dollar"><h2>{`= ${(dataCon.ballance*dataCon.exchangeValue).toFixed(2)}$`}</h2></div>
+    <div id="dollar"><h2>{`= ${exchangeValue===NaN ? "-" :exchangeValue}$`}</h2></div>
       </div>
 
       <TradeButtons/>
